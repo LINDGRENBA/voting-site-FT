0448ab125e6d307cebbdad044794cdad6e958a97
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-04 10:07:00 -0700

    Update HTML in index, update scripts.js with branching.
    Co-authored by: Mark Tiapkov <mark.tiapkov@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  let userAgeInput = parseInt(prompt('Please enter your age'));
+
+  if(userAgeInput >= 18) {
+    $("#eighteen-up").show();
+  } else {
+    $("#under-eighteen").show()
+  }
+
+});
